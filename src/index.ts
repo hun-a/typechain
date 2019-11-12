323b81ef1b09eb3a20aa34312930dd872f811175
hun-a <seunghun.chan@gmail.com>
2019-11-12 14:17:35 +0900

    Study - interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+interface Human {
+  name: string;
+  age: number;
+  gender: String;
+};
+
+const person: Human = {
+  name: 'huna',
+  age: 33,
+  gender: 'male'
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, your are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
