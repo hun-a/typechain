38af744ef56bdf720b4367c7f6d25489d353103f
hun-a <seunghun.chan@gmail.com>
2019-11-12 14:38:09 +0900

    Create some functions to handle the blocks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calcuateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
@@ -18,4 +22,8 @@ const genesisBlock: Block = new Block(0, '20200201012030405012020', '', 'Hello!'
 
 let blockchain: Array<Block> = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Array<Block> => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
