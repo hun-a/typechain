d8f00af8f9e093694ac9b10fd89aa7d7145e8145
hun-a <seunghun.chan@gmail.com>
2019-11-12 15:14:57 +0900

    Add the validation functions for block

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ class Block {
 
   static calcuateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === 'number' &&
+    typeof aBlock.hash === 'string' &&
+    typeof aBlock.previousHash === 'string' &&
+    typeof aBlock.timestamp === 'number' &&
+    typeof aBlock.data === 'string';
+
   constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
     this.hash = hash;
@@ -34,8 +41,19 @@ const createNewBlock = (data: string): Block => {
   const nextTimestamp: number = getNewTimestamp();
   const nextHash: string = Block.calcuateBlockHash(newIndex, previousBlock.hash, nextTimestamp, data);
   const newBlock: Block = new Block(newIndex, nextHash, previousBlock.hash, data, nextTimestamp);
-  blockchain.push(newBlock);
   return newBlock;
 };
 
-console.log(createNewBlock('Hello'), createNewBlock('bye bye'));
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  }  else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (Block.calcuateBlockHash(candidateBlock.index, previousBlock.hash, candidateBlock.timestamp, candidateBlock.data) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
